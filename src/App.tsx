f8c2163d9946a56e1b5ceb54023170a0bb801e2d
marcosvbueno <mvbueno07@gmail.com>
2025-07-30 16:13:38 -0300

    feat: implement file upload functionality with drag-and-drop support

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { cn } from './lib/utils';
+import { PackageOpenIcon, Trash2Icon } from 'lucide-react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [files, setFiles] = useState<File[]>([]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: acceptedFiles => {
+      setFiles(prevState => prevState.concat(acceptedFiles));
+    },
+  });
+
+  function handleRemoveFile(removingIndex: number) {
+    setFiles(prevState => {
+      const newFiles = [...prevState];
+      newFiles.splice(removingIndex, 1);
+      return newFiles;
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex justify-center py-20 px-6">
+      <div className="w-full max-w-xl">
+        <div
+          {...getRootProps()}
+          className={cn(
+            'border h-60 w-full border-dashed transition-colors flex items-center justify-center flex-col cursor-pointer',
+            isDragActive && 'bg-accent/50'
+          )}
+        >
+          <input {...getInputProps()} />
+          <div className="transform transition-all mb-2">
+            <PackageOpenIcon className="size-10 stroke-1 mb-2" />
+          </div>
+
+          <span>Solte seus arquivos aqui</span>
+          <small className="text-muted-foreground">
+            Apenas arquivos PNG de até 1MB
+          </small>
+        </div>
+
+        {files.length > 0 && (
+          <div className='mt-10'>
+            <h2 className='font-medium text-2xl tracking-tight'>Arquivos selecionados</h2>
+
+            <div className='mt-4 space-y-2'>
+              {files.map((file, index) => (
+              <div
+                key={index} className='border p-3 rounded-md flex justify-between items-center'>
+                  <span className='text-sm '>
+                    {file.name}
+                    </span>
+
+                    <Button variant={"destructive"} size={"icon"} onClick={() => handleRemoveFile(index)}>
+                    <Trash2Icon className='size-4' />
+                    </Button>
+                </div>
+            ))}
+
+            </div>
+            <Button className='mt-4 w-full cursor-pointer' size='lg'>
+              Upload
+            </Button>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
